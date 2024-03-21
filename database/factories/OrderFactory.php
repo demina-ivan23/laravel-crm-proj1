c43f7aca49e174b47a3cdd63dd94872333cce739
Sam de Vitt <semen.taninn@gmail.com>
2024-03-21 16:20:31 +0200

    order seeder available

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Prospect;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $customerId = $this->faker->numberBetween(Prospect::min('id'), Prospect::max('id'));
+        $customer = Prospect::find($customerId);
+        return [
+            'customer_id' => $customerId,
+            'customer_name' => $customer->name ?? $this->faker->name,
+            'customer_email' => $customer->email ?? $this->faker->email,
+        ];
+    }
+}
---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\{
+    Product, 
+    Order,
+};
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $orders = Order::factory(30)->create();
+        $orders->each(function ($order) {
+            $products = Product::inRandomOrder()->limit(rand(1, 5))->get();
+            $products->each(function ($product) use ($order) {
+                $order->products()->attach($product);
+            });
+        });
+    }
+}
