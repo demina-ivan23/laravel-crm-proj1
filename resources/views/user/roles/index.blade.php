e2c5f02900b0fae1c93ab19bf4efe90e859354d8
Sam de Vitt <semen.taninn@gmail.com>
2024-05-25 21:28:52 +0300

    index page for roles

---
+++ resources/views/user/roles/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            @if ($roles->count())
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                <h5>
+                                    <strong>Title</strong>
+                                </h5>
+                            </th>
+                            <th>
+                                <h5>
+                                    <strong>Permissions</strong>
+                                </h5>
+                            </th>
+                            <th>
+                                <h5>
+                                    <strong>Actions</strong>
+                                </h5>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($roles as $role)
+                            <tr>
+                                <th>
+                                    <h5>
+                                        <strong>{{ $role->title }}</strong>
+                                    </h5>
+                                </th>
+                                <th>
+                                    @if ($role->permissions->count())
+                                        @foreach ($role->permissions()->limit(3)->get() as $permission)
+                                            {{ $permission->title . ', ' }}
+                                        @endforeach
+                                        @if ($role->permissions->count() > 3)
+                                            and more
+                                        @endif
+                                    @else
+                                            No permissions yet
+                                    @endif
+                                </th>
+                                <th>
+                                    <div class="d-flex justify-content-evenly">
+                                        <a class="btn btn-secondary mr-4 pt-1 pb-1"
+                                            href="{{ route('user.roles.show', ['role' => $role]) }}">View</a>
+                                        <a class="btn btn-primary mr-4 pt-1 pb-1"
+                                            href="{{ route('user.roles.edit', ['role' => $role]) }}">Edit</a>
+                                    </div>
+                                </th>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <h2 class="d-flex justify-content-center">
+                    No roles that would fulfill the requirements of the applied filters yet. Create new roles or ask a
+                    person with a proper permissions to do it
+                </h2>
+            @endif
+        </div>
+    </div>
+@endsection
