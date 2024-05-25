f474259f33e45f758afc3bf19c0f53421ed24b3e
Sam de Vitt <semen.taninn@gmail.com>
2024-05-25 21:57:16 +0300

    roles controller

---
+++ app/Http/Controllers/User/Roles/RolesController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\User\Roles;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('user.roles.index', ['roles' => Role::latest()->filter()->paginate(20)]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user.roles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Role $role)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Role $role)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Role $role)
+    {
+        //
+    }
+
+    public function delete(Role $role)
+    {
+        //
+    }
+}
