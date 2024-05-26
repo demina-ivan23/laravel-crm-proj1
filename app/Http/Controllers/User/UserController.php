3c73f510efd47339690e1eaaab322dbbaa4e8185
Sam de Vitt <semen.taninn@gmail.com>
2024-05-26 07:26:24 +0300

    pagination error fixed

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public function index()
     {
-        return view('users.dashboard', User::latest()->filter()->paginate()->get());
+        return view('users.dashboard', User::latest()->filter()->paginate(10));
     }
 
     public function create()
