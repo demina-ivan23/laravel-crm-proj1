26d9cb4fe0c78874f439eed5583475930499ceb9
demina-ivan23 <demina.ivann@gmail.com>
2024-05-26 15:09:45 +0300

    store role request

---
+++ app/Http/Requests/Roles/StoreRoleRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Roles;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:225|unique:roles,title',
+            'description' => 'nullable|max:225',
+            'permissions' => 'array'
+        ];
+    }
+}
