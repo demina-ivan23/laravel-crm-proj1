f210d7337c5aaec612d0b1dfafa1d12b60359097
Sam de Vitt <semen.taninn@gmail.com>
2024-05-26 07:26:42 +0300

    role seeder updated

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Role;
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -13,17 +14,48 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create([
-            'title' => 'role 1',
-            'description' => 'a first placeholder role'
+        $observer = Role::create([
+            'title' => 'Observer',
+            'description' => 'observer'
         ]);
-        Role::create([
-            'title' => 'role 2',
-            'description' => 'a second placeholder role'
+        $writer = Role::create([
+            'title' => 'Writer',
+            'description' => 'writer'
         ]);
-        Role::create([
-            'title' => 'role 3',
-            'description' => 'a third placeholder role'
+        $editor = Role::create([
+            'title' => 'Editor',
+            'description' => 'editor'
         ]);
+        $admin = Role::create([
+            'title' => 'Admin',
+            'description' => 'admin'
+        ]);
+        $superadmin = Role::create([
+            'title' => 'Superadmin',
+            'description' => 'superadmin'
+        ]);
+        foreach (Permission::all() as $permission) {
+            $superadmin->permissions()->attach($permission->id);
+            if ($permission->title !== 'be_untouchable') {
+                $admin->permissions()->attach($permission->id);
+            }
+        }
+        $arrayForObserver = ['prospect-read-web', 'prospect-read-api', 'product-read-web', 
+        'product-read-api', 'order-read-web', 'order-read-api', 'user-read-self-web', 'user-read-self-api'];
+        $arrayForWriter = ['prospect-write-web', 'prospect-write-api', 'product-write-web',
+         'product-write-api', 'order-write-web', 'order-write-api', 'user-write-self-web', 'user-write-self-api'];
+        $arrayForEditor = ['prospect-edit-web', 'prospect-edit-api', 'product-edit-web', 
+        'product-edit-api', 'order-edit-web', 'order-edit-api', 'user-edit-self-web', 'user-edit-self-api',
+         'prospect-read-web', 'prospect-read-api', 'product-read-web', 'product-read-api', 'order-read-web', 
+         'order-read-api', 'user-read-self-web', 'user-read-self-api'];
+         foreach($arrayForObserver as $forObserver){
+            $observer->permissions()->attach(Permission::where('title', $forObserver)->first());
+         }
+         foreach($arrayForWriter as $forWriter){
+            $writer->permissions()->attach(Permission::where('title', $forWriter)->first());
+         }
+         foreach($arrayForEditor as $forEditor){
+            $editor->permissions()->attach(Permission::where('title', $forEditor)->first());
+         }
     }
 }
