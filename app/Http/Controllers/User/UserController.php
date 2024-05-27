366b43c6e9f77bd5a4f7058152dcedd5a2108bde
demina-ivan23 <demina.ivann@gmail.com>
2024-05-27 16:04:52 +0300

    index not dashboard

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public function index()
     {
-        return view('users.dashboard', User::latest()->filter()->paginate(10));
+        return view('users.index', User::latest()->filter()->paginate(10));
     }
 
     public function create()
