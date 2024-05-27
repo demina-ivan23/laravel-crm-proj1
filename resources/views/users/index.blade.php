e767b1f2c6ca472a21bf57bd4efc4b5c4dc508fc
demina-ivan23 <demina.ivann@gmail.com>
2024-05-27 21:03:34 +0300

    session messages

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
         <div class="card mt-4">
-
             <table class="table">
                 <thead>
                     <tr>
@@ -23,7 +32,7 @@
                         <th>
                             <h4>
                                 <strong>
-                                    Network status 
+                                    Network status
                                 </strong>
                             </h4>
                         </th>
@@ -41,29 +50,28 @@
                         <tr>
                             <th>
                                 <h5>
-                                    {{$user->name}}
+                                    {{ $user->name }}
                                 </h5>
                             </th>
                             <th>
                                 <h5>
-                                    {{$user->email}}
+                                    {{ $user->email }}
                                 </h5>
                             </th>
                             <th>
                                 <h5 class="d-flex justify-content-center">
-                                    {{$user->lastSeenOnline}}
+                                    {{ $user->lastSeenOnline }}
                                 </h5>
                             </th>
                             <th>
                                 <div class="d-flex justify-content-evenly">
-                                    <a href="{{route('users.show', ['user' => $user])}}" class="btn btn-secondary">View</a>
-                                    <a href="{{route('users.edit', ['user' => $user])}}" class="btn btn-primary">Edit</a>
-                                    <form action="{{route('users.delete', ['user' => $user])}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="submit" class="btn btn-danger" value="Delete">
+                                    <a href="{{ route('users.show', ['user' => $user]) }}" class="btn btn-secondary">View</a>
+                                    <a href="{{ route('users.edit', ['user' => $user]) }}" class="btn btn-primary">Edit</a>
+                                    <form action="{{ route('users.delete', ['user' => $user]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" class="btn btn-danger" value="Delete">
                                     </form>
-
                                 </div>
                             </th>
                         </tr>
