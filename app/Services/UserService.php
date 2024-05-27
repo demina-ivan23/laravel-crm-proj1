300e33fb185f1169cbcadae1de8794607c6f279b
demina-ivan23 <demina.ivann@gmail.com>
2024-05-27 16:05:20 +0300

    user updating and some of storing logic changed

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,28 +1,38 @@
-<?php 
+<?php
 
 namespace App\Services;
+
 use Exception;
 use App\Models\User;
 use Illuminate\Support\Str;
 
-class UserService {
-    static function storeUser(array $data){
+class UserService
+{
+
+    static function storeUser(array $data)
+    {
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'api_key' => Str::uuid(),
+            'api_key' => $data['api_key'] ?? Str::uuid(),
+            'role_id' => $data['role_id']
         ]);
         return $user;
-        
     }
-    static function updateUser(User $user, array $data){
-        try{
-            $user->update($data);
+    static function updateUser(User $user, array $data)
+    {
+        try {
+            $user->update([
+                'name' => $data['name'] ?? $user->name,
+                'email' => $data['email'] ?? $user->email,
+                'password' => bcrypt($data['password'] ?? null) ?? $user->password,
+                'api_key' => $data['api_key'] ?? $user->api_key,
+                'role_id' => $data['role_id']
+            ]);
             return true;
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
-        
     }
 }
