38b04a26b0baa78116fdb108a0606b5afaf24fde
demina-ivan23 <demina.ivann@gmail.com>
2024-05-27 17:06:30 +0300

    fix UserController::index

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public function index()
     {
-        return view('users.index', User::latest()->filter()->paginate(10));
+        return view('users.index', ['users' => User::latest()->filter()->paginate(10)]);
     }
 
     public function create()
