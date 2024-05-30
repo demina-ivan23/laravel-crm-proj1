b8967b0830d48f41eb9789ede72f189df96ac8d0
school-10 <my.school.10.2024@gmail.com>
2024-05-30 11:59:13 +0300

    dashboard tabulation to be finished

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,7 +11,28 @@
                 {{ session('success') }}
             </div>
         @endif
-        <div class="card mt-4">
+        <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+            <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="-tab"
+                data-tabs-toggle="#default-tab-content" role="tablist">
+                <li class="me-2" role="presentation">
+                    <button class="inline-block p-4 border-b-2 rounded-t-lg" id="roles-tab" data-tabs-target="#roles"
+                        type="button" role="tab" aria-controls="roles" aria-selected="false">Roles</button>
+                </li>
+                <li class="me-2" role="presentation">
+                    <button
+                        class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                        id="users-tab" data-tabs-target="#users" type="button" role="tab" aria-controls="users"
+                        aria-selected="false">Users</button>
+                </li>
+            </ul>
+        </div>
+        <div id="default-tab-content">
+        </div>
+        <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="roles" role="tabpanel"
+            aria-labelledby="roles-tab">
+        </div>
+        <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="users" role="tabpanel"
+            aria-labelledby="users-tab">
             <table class="table">
                 <thead>
                     <tr>
@@ -66,19 +87,21 @@
                             <th>
                                 <div class="d-flex justify-content-evenly">
                                     @if ($user->deleted_at)
-                                    <form action="{{ route('users.restore', ['user' => $user]) }}" method="POST">
-                                        @csrf
-                                        @method('PUT')
-                                        <input type="submit" class="btn btn-success" value="Restore">
-                                    </form>
-                                    @else 
-                                    <a href="{{ route('users.show', ['user' => $user]) }}" class="btn btn-secondary">View</a>
-                                    <a href="{{ route('users.edit', ['user' => $user]) }}" class="btn btn-primary">Edit</a>
-                                    <form action="{{ route('users.delete', ['user' => $user]) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <input type="submit" class="btn btn-danger" value="Delete">
-                                    </form>
+                                        <form action="{{ route('users.restore', ['user' => $user]) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <input type="submit" class="btn btn-success" value="Restore">
+                                        </form>
+                                    @else
+                                        <a href="{{ route('users.show', ['user' => $user]) }}"
+                                            class="btn btn-secondary">View</a>
+                                        <a href="{{ route('users.edit', ['user' => $user]) }}"
+                                            class="btn btn-primary">Edit</a>
+                                        <form action="{{ route('users.delete', ['user' => $user]) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input type="submit" class="btn btn-danger" value="Delete">
+                                        </form>
                                     @endif
                                 </div>
                             </th>
@@ -87,5 +110,6 @@
                 </tbody>
             </table>
         </div>
+    </div>
     </div>
 @endsection
