4d73774bd81f2891ca87c5ead98776a7c0f6cc23
demina-ivan23 <demina.ivann@gmail.com>
2024-06-02 14:08:11 +0300

    some explanation add

--- resources/views/dashboards/order-charts.blade.php
+++ resources/views/dashboards/order-charts.blade.php
@@ -31,14 +31,6 @@
                         </h5>
                     </button>
                 </li>
-                <li>
-                    <button class="p-4 border-b-2 hover:text-blue-600 hover:border-blue-400 tab-button"
-                        @click="togglePageTabs('prospectChart', 'prospectChartTabToggle')" id="prospectChartTabToggle">
-                        <h5>
-                            Prospect Chart
-                        </h5>
-                    </button>
-                </li>
             </ul>
         </div>
 
@@ -95,6 +87,7 @@
                     </div>
                 </div>
             </div>
+            <h5 class="d-flex justify-content-center mt-4">This chart shows how much orders where created with each status.</h5>
         </div>
         <div class="hidden tablink" id="productChart">
             <div class="card mt-4">
@@ -147,61 +140,7 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="hidden tablink" id="prospectChart">
-            <div class="card mt-4">
-                <div class="card-body">
-                    <div class="row">
-                        <h5 class="mb-3 d-flex justify-content-center">Prospect-related chart</h5>
-    
-                        <div class="col-sm-3">
-                            <form action="" method="GET">
-                                <h5 class="mb-3">From/To period</h5>
-                                <div class="mb-2">
-                                    <label for="order_prospect_chart_from">From</label>
-                                    <input class="form-control" type="date" name="order_prospect_chart_from"
-                                        id="order_prospect_chart_from">
-                                </div>
-    
-                                <div class="mb-4">
-                                    <label for="order_prospect_chart_to">To</label>
-                                    <input class="form-control" type="date" name="order_prospect_chart_to"
-                                        id="order_prospect_chart_to">
-                                </div>
-                                <div class="mb-2">
-                                    <label for="prospect_state">Prospect State</label>
-                                    <select class="form-control" name="prospect_state" id="prospect_state">
-                                        @php
-                                            use App\Models\ProspectState;
-                                            $prospect_states = ProspectState::all();
-                                        @endphp
-                                        <option value="all">All</option>
-                                        @foreach ($prospect_states as $prospect_state)
-                                            <option value="{{ $prospect_state->id }}">{{ $prospect_state->title }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-    
-                                <button class="btn btn-primary pt-0 pb-0" type="submit">Submit</button>
-                            </form>
-                        </div>
-                        <div class="col-sm-9">
-                            <div id="order_prospect_data_wrapper" hidden>
-                                @php
-                                    $jsonData = json_encode($data['order_prospect_chart_info']);
-                                @endphp
-                                <input type="hidden" id="order_prospect_data" value="{{ $jsonData }}">
-                            </div>
-                            <div data-v-409fe714 class="chart-view">
-                                <canvas style="display: block; box-sizing: border-box; height: 228px; width: 456px;"
-                                    id="prospectOrderChartCanvas">
-    
-                                </canvas>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h5 class="d-flex justify-content-center mt-4">This chart shows how much products of which category where ordered, also showing the number of these orders for each status.</h5>
         </div>
     </div>
 @endsection
