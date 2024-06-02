8cce81be1d194a02bf23a51ec9baca4c386dac6e
demina-ivan23 <demina.ivann@gmail.com>
2024-06-02 19:43:22 +0300

    OrderStatusPolicy create

---
+++ app/Policies/OrderStatusPolicy.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class OrderStatusPolicy
+{
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user): bool
+    {
+        if($user->role && $user->role->permissions->contains('order_status-read-web')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        if($user->role && $user->role->permissions->contains('order_status-write-web')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user): bool
+    {
+        if($user->role && $user->role->permissions->contains('order_status-edit-web')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user): bool
+    {
+        if($user->role && $user->role->permissions->contains('order_status-edit-web')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user): bool
+    {
+        if($user->role && $user->role->permissions->contains('order_status-edit-web')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user): bool
+    {
+        if($user->role && $user->role->permissions->contains('order_status-delete-web')){
+            return true;
+        }
+        return false;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use App\Enums\UserApiAccessLevelEnum;
 use App\Policies\OrderPolicy;
+use App\Policies\OrderStatusPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         OrderPolicy::class,
-        
+        OrderStatusPolicy::class,
     ];
 
     /**
