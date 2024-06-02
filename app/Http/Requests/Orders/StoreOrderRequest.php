7c8c48664bf8649cb9fb3bfe844fe7b0a2bb0114
demina-ivan23 <demina.ivann@gmail.com>
2024-06-02 19:29:19 +0300

    permission check add

--- app/Http/Requests/Orders/StoreOrderRequest.php
+++ app/Http/Requests/Orders/StoreOrderRequest.php
@@ -11,7 +11,10 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        if(auth()->check() && auth()->user()->role->permissions->contains('order-write-web')){
+            return true;
+        }
+        return false;
     }
 
     /**
